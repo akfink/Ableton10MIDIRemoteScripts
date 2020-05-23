8c5a3e0e6ece3685205d4c50b0a1a3a02a2b4799
pH Balance <afink@entropycentral.net>
2020-05-23 01:30:00 -0700

    Initial file uploads

---
+++ Korg_NanoKon_NOPAN_Guida/__init__.py
@@ -0,0 +1,8 @@
+# http://remotescripts.blogspot.com
+
+from NanoKontrol_NoPan import NanoKontrol_NoPan
+
+def create_instance(c_instance):
+    """ Creates and returns the NanoKontrol script """
+    return NanoKontrol_NoPan(c_instance)
+
