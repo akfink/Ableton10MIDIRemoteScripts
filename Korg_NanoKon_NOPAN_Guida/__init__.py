3c501d45e65fffdd027386e0e79c685b3ffb35b3
pH Balance <afink@entropycentral.net>
2020-05-24 22:35:39 -0700

    updated all file prefs to 755

--- Korg_NanoKon_NOPAN_Guida/__init__.py
+++ Korg_NanoKon_NOPAN_Guida/__init__.py
@@ -1,8 +1,6 @@
-# http://remotescripts.blogspot.com
-
+from __future__ import absolute_import, print_function, unicode_literals, with_statement
 from NanoKontrol_NoPan import NanoKontrol_NoPan
 
 def create_instance(c_instance):
     """ Creates and returns the NanoKontrol script """
     return NanoKontrol_NoPan(c_instance)
-
