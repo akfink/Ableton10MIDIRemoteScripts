b3dca114261399492b33c7c8dfad9b2dd99c24b0
pH Balance <afink@entropycentral.net>
2020-05-27 18:53:52 -0700

    added rest of Oxy3Gen script body to see if it loads

--- AKF_Oxy49/AKF_Oxy49.py
+++ AKF_Oxy49/AKF_Oxy49.py
@@ -1,7 +1,5 @@
 # AKF custom script for M-Audio Oxygen 61 (blue)
-# NOTE: Script assumes MIDI mapping for factory preset 10, except with Buttons C1-C9 changed to toggle by removing CTRL ASSN = 146.
-
-
+# NOTE: Script assumes MIDI mapping for factory preset 10, with DirectLink disabled (somehow...)
 
 from __future__ import absolute_import, print_function, unicode_literals, with_statement
 import Live # This allows us (and the Framework methods) to use the Live API on occasion
@@ -29,23 +27,24 @@ from _Framework.EncoderElement import EncoderElement
 from _Framework.DeviceComponent import DeviceComponent
 from _Framework.CompoundElement import CompoundElement
 from _Framework.ControlElement import ControlElement
-#todo: remove unused classes
+from compound_element import compound_element
+
 
 """ Here we define some global variables """
 CHANNEL = 15 # Channels are numbered 0 through 15, this script only makes use of one MIDI Channel (Channel 1)
 session = None #Global session object - global so that we can manipulate the same session object from within our methods
 mixer = None #Global mixer object - global so that we can manipulate the same mixer object from within our methods
 
-class AKF_Oxy49(ControlSurface):
+class AKF_Oxy61(ControlSurface, ControlElement, CompoundElement, compound_element):
     __module__ = __name__
-    __doc__ = "MAudio Oxygen 49 MIDI Remote script "
+    __doc__ = "MAudio Oxygen 61 MIDI Remote script "
 
     def __init__(self, c_instance):
         with self.component_guard():
             ControlSurface.__init__(self, c_instance)
             self.log_message(time.strftime("%d.%m.%Y %H:%M:%S", time.localtime()) + "+ + ========= AKF_Oxy61 log opened ========= + +")
             # Writes message into Live's main log file. This is a ControlSurface method.
-            self.set_suppress_rebuild_requests(True) # Turn off rebuild MIDI map until after we're done setting up
+            self._set_suppress_rebuild_requests(True) # Turn off rebuild MIDI map until after we're done setting up
 
         #paste from Oxygen_3rd_Gen.py
             is_momentary = True
@@ -66,6 +65,56 @@ class AKF_Oxy49(ControlSurface):
             self._setup_mixer_control() # Setup the mixer object
             self._setup_session_control()  # Setup the session object
 
-            self.set_suppress_rebuild_requests(False) # Turn rebuild back on, once we're done setting up
 
-    def
+            self._set_suppress_rebuild_requests(False) # Turn rebuild back on, once we're done setting up
+
+            self._shift_value(0)
+            self._mixer.master_strip().set_volume_control(self._master_slider)
+            self._mixer.selected_strip().set_volume_control(None)
+            device = DeviceComponent(device_selection_follows_track_selection=True)
+            device.set_parameter_controls(tuple([ EncoderElement(MIDI_CC_TYPE, GLOBAL_CHANNEL, 17 + index, Live.MidiMap.MapMode.absolute) for index in range(8)
+                                                ]))
+            self.set_device_component(device)
+            ffwd_button = ButtonElement(is_momentary, MIDI_CC_TYPE, GLOBAL_CHANNEL, 115)
+            rwd_button = ButtonElement(is_momentary, MIDI_CC_TYPE, GLOBAL_CHANNEL, 114)
+            loop_button = ButtonElement(is_momentary, MIDI_CC_TYPE, GLOBAL_CHANNEL, 113)
+            transport = TransportComponent()
+            transport.set_stop_button(ButtonElement(is_momentary, MIDI_CC_TYPE, GLOBAL_CHANNEL, 116))
+            transport.set_play_button(ButtonElement(is_momentary, MIDI_CC_TYPE, GLOBAL_CHANNEL, 117))
+            transport.set_record_button(ButtonElement(is_momentary, MIDI_CC_TYPE, GLOBAL_CHANNEL, 118))
+            session = SessionComponent(0, 0)
+            transport_view_modes = TransportViewModeSelector(transport, session, ffwd_button, rwd_button, loop_button)
+        return
+
+    def disconnect(self):
+        self._shift_button.remove_value_listener(self._shift_value)
+        self._shift_button = None
+        ControlSurface.disconnect(self)
+        return
+
+    def refresh_state(self):
+        ControlSurface.refresh_state(self)
+        self.schedule_message(5, self._send_midi, IDENTITY_REQUEST)
+
+    def handle_sysex(self, midi_bytes):
+        if midi_bytes[0:5] == IDENTITY_RESPONSE:
+            if midi_bytes[10] == 38:
+                self._mixer.master_strip().set_volume_control(None)
+                self._mixer.selected_strip().set_volume_control(self._master_slider)
+        return
+
+    def _shift_value(self, value):
+        assert value in range(128)
+        for index in range(NUM_TRACKS):
+            if value == 0:
+                self._mixer.channel_strip(index).set_solo_button(None)
+                self._mixer.channel_strip(index).set_mute_button(self._mute_solo_buttons[index])
+                self._mixer.set_bank_buttons(None, None)
+                self._mixer.set_select_buttons(self._track_up_button, self._track_down_button)
+            else:
+                self._mixer.channel_strip(index).set_mute_button(None)
+                self._mixer.channel_strip(index).set_solo_button(self._mute_solo_buttons[index])
+                self._mixer.set_select_buttons(None, None)
+                self._mixer.set_bank_buttons(self._track_up_button, self._track_down_button)
+
+        return
